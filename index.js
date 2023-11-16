173d5e85a553247b106f29dd18aed2211ec8464b
Mary Sigmon <marysigmon@Marys-MacBook-Pro-2.local>
2023-11-16 14:31:59 -0500

    prompt completed

---
+++ index.js
@@ -0,0 +1,5 @@
+// Prompt the user for a list of froyo flavors separated by commas. 
+const flavorsString = prompt(
+    "Please enter a list of comma-separated froyo flavors.",
+    "vanilla, vanilla, vanilla, strawberry, coffee, coffee"
+);
